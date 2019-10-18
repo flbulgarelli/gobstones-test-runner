a09f949052f8b99d234ec18946625ab6ee11fb99
Franco Leonardo Bulgarelli <franco@mumuki.org>
2019-10-18 14:13:03 -0300

    Adding test

--- test/api-spec.js
+++ test/api-spec.js
@@ -68,6 +68,53 @@ describe("api", () => {
     });
   });
 
+  it("evaluates batches programatically with passed tests", () => {
+    const result = runner.runTests({
+      "code": "program { Poner(Rojo) }",
+      "examples": [
+        {
+          "initialBoard": "GBB/1.0\nsize 4 4\nhead 0 0",
+          "expectedBoard": "GBB/1.0\nsize 4 4\ncell 0 0 Rojo 1\nhead 0 0",
+        }
+      ]
+    })
+    assert.deepStrictEqual(result, {
+      "results":[
+        {
+          "status":"passed",
+          "result": {
+            "status":"passed",
+            "initialBoard": "GBB/1.0\nsize 4 4\nhead 0 0\n",
+            "expectedBoard": "GBB/1.0\nsize 4 4\ncell 0 0 Rojo 1\nhead 0 0\n",
+            "finalBoard": "GBB/1.0\nsize 4 4\ncell 0 0 Rojo 1\nhead 0 0\n",
+            "returnValue":null
+          }
+        }
+      ],
+      "mulangAst":{
+        "tag":"EntryPoint",
+        "contents":[
+          "program",
+          {
+            "tag":"Application",
+            "contents":[
+              {
+                "tag":"Reference",
+                "contents":"Poner"
+              },
+              [
+                {
+                  "tag":"MuSymbol",
+                  "contents":"Rojo"
+                }
+              ]
+            ]
+          }
+        ]
+      }
+    });
+  });
+
   it("evaluates batches with tables", () => {
     const result = runner.runTests({
       "code": "program { Meter(Rojo) }",
